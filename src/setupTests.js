3acea1e43c1068ab05d37977226495fcb4fe3c03
Jeff Golden <codebygolden@gmail.com>
2021-07-29 10:56:40 -0600

    Mock Service Worker setup.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+//import Mock Service Worker handlers
+import { server } from "./mocks/server.js";
+
+// establish API mocking before all tests
+beforeAll(() => server.listen());
+
+// reset any request handlers that we may add during tests
+afterEach(() => server.resetHandlers());
+
+// clean up after tests are finished
+afterAll(() => server.close());
